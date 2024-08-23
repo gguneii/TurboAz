1a725016590737bcb321a378e9d3e27a72d42b53
gguneii <guneysuleymanofa@gmail.com>
2024-08-23 17:42:05 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,330 @@
+const cars = [{
+        name: "Mercedes G 400",
+        image: "https://turbo.azstatic.com/uploads/full/2023%2F11%2F05%2F18%2F09%2F22%2F4ebebadc-66d7-4372-bc13-479c4817f26a%2F3815_EVRmwPRvT-t2ad98DQwJZQ.jpg",
+        price: 219999,
+        year: 2023
+    },
+    {
+        name: "Toyota Camry",
+        image: "https://turbo.azstatic.com/uploads/full/2023%2F11%2F05%2F18%2F09%2F22%2F4ebebadc-66d7-4372-bc13-479c4817f26a%2F3815_EVRmwPRvT-t2ad98DQwJZQ.jpg",
+        price: 25000,
+        year: 2021
+    },
+    {
+        name: "Mercedes G 400",
+        image: "https://turbo.azstatic.com/uploads/full/2023%2F11%2F05%2F18%2F09%2F22%2F4ebebadc-66d7-4372-bc13-479c4817f26a%2F3815_EVRmwPRvT-t2ad98DQwJZQ.jpg",
+        price: 219999,
+        year: 2023
+    },
+    {
+        name: "Toyota Camry",
+        image: "https://turbo.azstatic.com/uploads/full/2023%2F11%2F05%2F18%2F09%2F22%2F4ebebadc-66d7-4372-bc13-479c4817f26a%2F3815_EVRmwPRvT-t2ad98DQwJZQ.jpg",
+        price: 25000,
+        year: 2021
+    },
+    {
+        name: "Honda Accord",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F10%2F19%2F40%2F58%2F9f9979e7-9783-439f-96ce-6f7d477dac6c%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 24000,
+        year: 2020
+    },
+    {
+        name: "Tesla Model S",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F08%2F19%2F18%2F50%2F32%2F1786618f-7db0-4db0-9688-86166843fb1f%2F76402_Kt6M8xdUQ8mUvNPSzi54jw.jpg",
+        price: 80000,
+        year: 2022
+    },
+    {
+        name: "BMW 3 Series",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F08%2F18%2F21%2F32%2F09%2Ff22104fa-c0b1-4817-9dfa-04798302bf8c%2F26954_DWgEQQqRrb1W5yDQKFgsVA.jpg",
+        price: 35000,
+        year: 2019
+    },
+    {
+        name: "Audi A4 Ben",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F08%2F13%2F15%2F00%2F50%2F2b893f2a-ca14-41dd-8390-6256b7cc8664%2F11375_qXr7-N1ppBcS2a5TEaxWRQ.jpg",
+        price: 37000,
+        year: 2021
+    },
+    {
+        name: "Audi A4 Ben",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F08%2F13%2F15%2F00%2F50%2F2b893f2a-ca14-41dd-8390-6256b7cc8664%2F11375_qXr7-N1ppBcS2a5TEaxWRQ.jpg",
+        price: 37000,
+        year: 2021
+    },
+    {
+        name: "Mercedes-Benz",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F08%2F13%2F15%2F00%2F50%2F2b893f2a-ca14-41dd-8390-6256b7cc8664%2F11375_qXr7-N1ppBcS2a5TEaxWRQ.jpg",
+        price: 42000,
+        year: 2020
+    },
+    {
+        name: "Ford Mustang",
+        image: "https://images.prismic.io/carwow/c2d2e740-99e2-4faf-8cfa-b5a75c5037c0_ford-mustang-2024-lhd-front34static.jpg?auto=format&cs=tinysrgb&fit=crop&q=60&w=750",
+        price: 55000,
+        year: 2022
+    },
+    {
+        name: "Chevrolet Camaro",
+        image: "https://images.prismic.io/carwow/c2d2e740-99e2-4faf-8cfa-b5a75c5037c0_ford-mustang-2024-lhd-front34static.jpg?auto=format&cs=tinysrgb&fit=crop&q=60&w=750",
+        price: 48000,
+        year: 2021
+    },
+    {
+        name: "Chevrolet Camaro",
+        image: "https://images.prismic.io/carwow/c2d2e740-99e2-4faf-8cfa-b5a75c5037c0_ford-mustang-2024-lhd-front34static.jpg?auto=format&cs=tinysrgb&fit=crop&q=60&w=750",
+        price: 48000,
+        year: 2021
+    },
+    {
+        name: "Nissan Altima",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F18%2F11%2F53%2F28%2Fd864eb35-4c49-4c44-a0c1-b10a23d537cd%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 23000,
+        year: 2019
+    },
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+    {
+        name: "Hyundai Sonata",
+        image: "https://turbo.azstatic.com/uploads/full/2024%2F07%2F24%2F11%2F19%2F00%2F1b03adfd-854f-4e07-9eda-233736201d76%2F73879_Llsf7HE7T9ZVmMOurKtMGQ.jpg",
+        price: 22000,
+        year: 2020
+    },
+
+];
+
+
+const cards = document.querySelector('.cards')
+
+function show() {
+
+    cars.map((item) => {
+        cards.innerHTML +=
+            `
+             <div class="card  bg-white rounded-md shadow-lg">
+                <div class="cars w-full h-60">
+                    <img class="w-full h-full object-cover rounded-t-lg" src="${item.image}" alt="car">
+                </div>
+                <div class="texts p-4">
+                    <p class="font-bold text-lg">${item.price} $</p>
+                    <p class="font-medium text-md">${item.name}</p>
+                    <p>${item.year}, 2.0 L, 0 km</p>
+                    <p class="text-[#8d94ad]">Baki, bugun 22:01</p>
+                    
+
+                </div>
+              </div>
+            `;
+    })
+}
+show()
+
+const menu = document.getElementById('menu')
+const body = document.querySelector('body')
+
+function bas() {
+    menu.classList.toggle('open');
+    if (menu.classList.contains('open')) {
+        body.style.overflow = 'hidden';
+    } else {
+        body.style.overflow = '';
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{htm,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+}
+
